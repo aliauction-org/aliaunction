bf0d4d791c324a15e5cb760c0c3d1070c8410791
1234admi-hu-mere-bhai <kaliasgar776@gmail.com>
2026-01-13 15:49:27 +0530

    Update urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('confirm-payment/<int:payment_proof_id>/', views.confirm_payment_received, name='confirm_payment_received'),
     path('history/', views.payment_history, name='payment_history'),
     path('platform-details/', views.platform_payment_details, name='platform_payment_details'),
+    path("invoice/<int:auction_id>/", views.invoice_view, name="invoice_view"),
+    path("pay/<int:invoice_id>/", views.pay_invoice, name="pay_invoice"),
 ] 
