9cd910c67afbd26ba70767813ed10b9961bf5094
1234admi-hu-mere-bhai <kaliasgar776@gmail.com>
2026-01-13 13:40:28 +0530

    Update settings.py

--- aliaunction/settings.py
+++ aliaunction/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bid_protection.apps.BidProtectionConfig',
     'users',
     'auctions',
     'payments',
