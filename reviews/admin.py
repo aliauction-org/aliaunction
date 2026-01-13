bae396ef9c479ee3d8be61314a6778eaa5144ada
1234admi-hu-mere-bhai <kaliasgar776@gmail.com>
2026-01-13 18:00:16 +0530

    Update admin.py

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from .models import Review, Rating
 
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('auction', 'user', 'content', 'created_at')
-    list_filter = ('auction', 'user')
-    search_fields = ('auction__title', 'user__username', 'content')
-
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ('auction', 'user', 'score', 'created_at')
-    list_filter = ('auction', 'user')
-    search_fields = ('auction__title', 'user__username')
+    list_display = ("given_by", "given_to", "stars", "created_at")
+    list_filter = ("stars", "role")
