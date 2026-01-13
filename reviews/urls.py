212cdd8fecf65d2bd219bececaa7841ba05dd9b1
1234admi-hu-mere-bhai <kaliasgar776@gmail.com>
2026-01-13 17:45:47 +0530

    Update urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('submit/<int:auction_id>/', views.submit_review, name='submit_review'),
-    path('auction/<int:auction_id>/', views.auction_reviews, name='auction_reviews'),
-] 
+    path("<int:escrow_id>/", views.leave_rating, name="leave_rating"),
+]
